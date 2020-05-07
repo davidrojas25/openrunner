b696aad5b5d537c51f13a4e0340128d475bac0f1
David Rojas <colombianclimber@gmail.com>
2020-05-07 10:30:16 -0700

    openlapace

--- openLapPace.py
+++ openLapPace.py
@@ -6,8 +6,16 @@ def kilometers_to_miles(kilometers):
   miles = kilometers * conv_fac
   print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
 
+def miles_to_laps(miles):
+    laps = 0.00
+    laps = miles * 4
+    return laps
 
 
 kilometers_to_miles(float(input("Enter value in kilometers: ")))
 
-
+miles1 = float(input("Enter the number of miles: "))
+laps1 = miles_to_laps(miles1)
+print("Your Track race will have " + str(laps1))          
+          
+          
