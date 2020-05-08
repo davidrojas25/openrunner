8133c28cf1e5fb4c7a3fbce5f983a2d10863e69a
David Rojas <colombianclimber@gmail.com>
2020-05-07 20:45:14 -0700

    images

--- images.py
+++ images.py
@@ -3,17 +3,59 @@ from PIL import ImageTk, Image
 
 root = Tk()
 root.title("Learn to Code")
-root.iconbitmap("giticon.ico")
+root.iconbitmap("images/giticon.ico")
 
-my_img = ImageTk.PhotoImage(Image.open("warriors xc2019.jpg"))
-my_label = Label(image=my_img)
-my_label.pack()
+my_img1 = ImageTk.PhotoImage(Image.open("images/warriors xc2019.jpg"))
+my_img2 = ImageTk.PhotoImage(Image.open("images/racegirls.jpg"))
+my_img3 = ImageTk.PhotoImage(Image.open("images/racegirls.jpg"))
+my_img4 = ImageTk.PhotoImage(Image.open("images/spook23.GIF"))
+my_img5 = ImageTk.PhotoImage(Image.open("images/warriorspract1.jpg"))
+
+image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+
+my_label = Label(image=my_img1)
+my_label.grid(row=0, column=0, columnspan=3)
+
+def forward(image_num):
+    global my_label
+    global button_forward
+    global button_back
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_num-1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_num + 1))
+    button_back = Button(root, text="<<", command=lambda: back(image_num - 1))
+    if image_num == 5:
+        button_forward = Button(root, text=">>", state=DISABLED)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
+    my_label.grid(row=0, column=0, columnspan=3)
+def back(image_num):
+    global my_label
+    global button_forward
+    global button_back
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_num-1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_num + 1))
+    button_back = Button(root, text="<<", command=lambda: back(image_num - 1))
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
+    my_label.grid(row=0, column=0, columnspan=3)
+
+button_back = Button(root, text="<<", command=lambda: back)
+button_exit = Button(root, text="Exit Program", command=root.quit)
+button_forward = Button(root, text=">>", command=lambda: forward(2))
+
+button_back.grid(row=1, column=0)
+button_exit.grid(row=1, column=1)
+button_forward.grid(row=1, column=2)
 
 
 
 
-button_quit = Button(root, text="Exit Program", command=root.quit)
-button_quit.pack()
+
+
+
+
 
 
 
